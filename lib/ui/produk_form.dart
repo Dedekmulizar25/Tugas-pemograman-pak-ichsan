f4eedb7564c6fa8e3379c752cfc45d904c2ca08a
dedekmulizarramadhan <dedekmulizar48@gmail.com>
2022-11-08 17:58:24 +0700

    upload tugas ke git

---
+++ lib/ui/produk_form.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ProdukForm extends StatefulWidget {
+  const ProdukForm({super.key});
+
+  @override
+  State<ProdukForm> createState() => _ProdukFormState();
+}
+
+class _ProdukFormState extends State<ProdukForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Form produk'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          TextField(decoration: const InputDecoration(labelText: 'kode produk')),
+          TextField(decoration: const InputDecoration(labelText: 'nama produk')),
+          TextField(decoration: const InputDecoration(labelText: 'harga produk')),
+          ElevatedButton(onPressed: () {}, child: const Text('simpan'))
+        ]),
+      ),
+    );
+  }
+}
